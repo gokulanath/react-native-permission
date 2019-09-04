fc512dd18b3b4ad02cbc5c7ec8d6126b94991e67
Gokul Shanmugam <gokul@jobfilez.com>
2019-09-03 18:56:00 -0700

    Removed unused permission

---
+++ ios/RCTConvert+RNPStatus.m
@@ -0,0 +1,19 @@
+//
+//  RCTConvert+RNPermissionsStatus.m
+//  ReactNativePermissions
+//
+//  Created by Yonah Forst on 23/03/16.
+//  Copyright © 2016 Yonah Forst. All rights reserved.
+//
+
+#import "RCTConvert+RNPStatus.h"
+
+@implementation RCTConvert (RNPStatus)
+
+RCT_ENUM_CONVERTER(RNPType, (@{ @"location" : @(RNPTypeLocation),
+                                @"backgroundRefresh": @(RNPTypeBackgroundRefresh),
+                                @"motion": @(RNPTypeMotion)
+                                }),
+                                RNPTypeUnknown, integerValue)
+
+@end
